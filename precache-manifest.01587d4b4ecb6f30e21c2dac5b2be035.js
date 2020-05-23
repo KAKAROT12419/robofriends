b579f65bd3b3fdc5d77868a7ed7c2e8a14b4f3fe
KAKAROT12419 <64268010+KAKAROT12419@users.noreply.github.com>
2020-05-23 21:46:00 +0530

    Updates

---
+++ precache-manifest.01587d4b4ecb6f30e21c2dac5b2be035.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73ab648804244f74c3cecf36d2db562",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3a74483f871a0506079d",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a7f125d1faf49fc8c55d",
+    "url": "/robofriends/static/css/main.fe7444fa.chunk.css"
+  },
+  {
+    "revision": "3a74483f871a0506079d",
+    "url": "/robofriends/static/js/2.20d3aaa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.20d3aaa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f125d1faf49fc8c55d",
+    "url": "/robofriends/static/js/main.c0815b28.chunk.js"
+  },
+  {
+    "revision": "bb8266954e45eefb9448",
+    "url": "/robofriends/static/js/runtime-main.99ad1e48.js"
+  }
+]);
